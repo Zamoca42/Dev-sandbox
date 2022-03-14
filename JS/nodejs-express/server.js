ae780298866d07bd55fb4cb4a6e1f2e43b42b533
Zamoca <suntail2002@naver.com>
2022-03-14 20:38:02 +0900

    Today I Learned nodejs

--- JS/nodejs-express/server.js
+++ JS/nodejs-express/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const bodyParser= require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}));
 
 //listen(서버를 오픈할 포트번호, function(){서버 오픈시 실행할 코드})
 app.listen(8080, function() {
@@ -18,4 +19,14 @@ app.get('/beauty', function(request, response){
 
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/write', function(request, response){
+    response.sendFile(__dirname + '/write.html');
+});
+
+//어떤 사람이 /add 경로로 POST 요청을 하면...
+app.post('/add', function(request, response){
+    response.send('전송완료');
+    console.log(request.body);
 });

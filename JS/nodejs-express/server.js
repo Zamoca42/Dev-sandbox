0b1cdd1c9562daaa53a8cadc485ca678e5c9f3be
Zamoca <suntail2002@naver.com>
2022-03-15 21:40:50 +0900

    Today I Learned express

--- JS/nodejs-express/server.js
+++ JS/nodejs-express/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const app = express();
+const db_config = require('./db_config.json');
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}));
+const MongoClient = require('mongodb').MongoClient;
 
-//listen(서버를 오픈할 포트번호, function(){서버 오픈시 실행할 코드})
-app.listen(8080, function() {
-    console.log('listening on 8080')
-});
+var db;
+MongoClient.connect(`${db_config.database}`, function(error, client){
+    //연결되면 할 일
+    if (error) return console.log(error)
+    //데이터베이스에 연결
+    db = client.db('todoapp');
+
+    // db.collection('post').insertOne( {_id: 100, 이름 : 'John', 나이 : 20}, function(error, result){
+    //     console.log('저장완료');
+    // });
+
+    //listen(서버를 오픈할 포트번호, function(){서버 오픈시 실행할 코드})
+    app.listen(8080, function() {
+        console.log('listening on 8080')
+    });
+})
+
 
 //누군가 /pet으로 방문을 하면 pet관련 안내문을 띄워주자
 app.get('/pet', function(request, response){
@@ -28,5 +43,7 @@ app.get('/write', function(request, response){
 //어떤 사람이 /add 경로로 POST 요청을 하면...
 app.post('/add', function(request, response){
     response.send('전송완료');
-    console.log(request.body);
+    db.collection('post').insertOne( {날짜 : `${request.body.date}`, 제목 : `${request.body.title}`}, function(error, result){
+        console.log('저장완료');
+    });
 });

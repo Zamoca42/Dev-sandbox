ca5d5926ff770b99f61fc5978aab91a6af795917
Zamoca <suntail2002@naver.com>
2022-03-18 00:27:29 +0900

    Today I Learned node

--- JS/nodejs-express/server.js
+++ JS/nodejs-express/server.js
@@ -45,9 +45,23 @@ app.get('/write', function(request, response){
 //어떤 사람이 /add 경로로 POST 요청을 하면...
 app.post('/add', function(request, response){
     response.send('전송완료');
-    db.collection('post').insertOne( {날짜 : request.body.date, 제목 : request.body.title}, function(error, result){
-        console.log('저장완료');
-    });
+    //쿼리문과 비슷함 - counter에서 게시물 갯수라는 항목을 찾는다
+    db.collection('counter').findOne({name : '게시물갯수'}, function(error,result){
+        //총 게시물 갯수를 변수에 저장
+        var totalPosts = result.totalPost;
+
+        //DB에 저장
+        db.collection('post').insertOne( { _id : totalPosts + 1, 날짜 : request.body.date, 제목 : request.body.title}, function(error, result){
+            console.log('저장완료');
+            //게시물 갯수 id항목 1증가 - updateOne은 수정
+            db.collection('counter').updateOne( {name:'게시물갯수'},{ $inc : {totalPost:1} }, function(error, result){
+                //$set, $inc은 update전용 operator
+                if(error){return console.log('error')}
+                console.log('수정완료')
+            });
+        });
+        
+    }); 
 });
 
 // /list 로 get요청으로 실제 DB에 저장된 테이터들로 접속하면 HTML을 보여줌

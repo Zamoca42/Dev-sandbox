123df89573fc999211b119bd001eff84fb848ccd
Zamoca <suntail2002@naver.com>
2022-03-28 23:47:31 +0900

    Today I Learned React

--- JS/React-class/shop/src/Detail.js
+++ JS/React-class/shop/src/Detail.js
@@ -1,20 +1,21 @@
 import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
-function Detail(){
+function Detail(props){
 
+    let { id } = useParams(); //라운터의 useParams 훅
     let history = useHistory();
 
     return (
         <div className="container">
         <div className="row">
           <div className="col-md-6">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+            <img src={'https://codingapple1.github.io/shop/shoes1.jpg'} width="100%" />
           </div>
           <div className="col-md-6 mt-4">
-            <h4 className="pt-5">상품명</h4>
-            <p>상품설명</p>
-            <p>120000원</p>
+            <h4 className="pt-5">{props.shoes[id].title}</h4>
+            <p>{props.shoes[id].content}</p>
+            <p>{props.shoes[id].price}원</p>
             <button className="btn btn-danger">주문하기</button> 
             <button className="btn btn-primary" onClick={ ()=>{
                 history.goBack(); //뒤로가기

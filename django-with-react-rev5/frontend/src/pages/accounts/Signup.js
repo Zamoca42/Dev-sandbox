a80471b46d4e15969d16474689305d4a98bb198c
Zamoca <suntail2002@naver.com>
2022-06-12 21:43:12 +0900

    add react antdesign signup form\

--- django-with-react-rev5/frontend/src/pages/accounts/Signup.js
+++ django-with-react-rev5/frontend/src/pages/accounts/Signup.js
@@ -1,38 +1,71 @@
-import { useState } from "react";
+import { useState, useHistory } from "react";
 import Axios from "axios";
+import { Button, Checkbox, Form, Input } from 'antd';
 
 export default function Signup() {
-    const [inputs, setInputs] = useState({});
-    //const [username, setUsername] = useState("");
-    //const [password, setPassword] = useState("");
 
-    const onSubmit = e => {
-        e.prventDefault();
+    const onFinish = (values) => {
+        const { username, password } = values
 
-        Axios.post("http://localhost:8000/accounts/signup/", inputs)
-            .then(response => {
-                console.log("response :", response);
-            })
-            .catch(error => {
-                console.log("error :", error);
-            })
-        console.log("onSubmit :", inputs);
-    }
-    
-    const onChange = e => {
-        const { name, value } = e.target
-        setInputs(prev => ({
-            ...prev,
-            [name]: value
-        }))
-    }
+        const data = { username, password};
+        Axios.post("http://localhost:8000/accounts/signup/", data);
+      }
+    const onFinishFailed = (errorInfo) => {
+        console.log('Failed:', errorInfo);
+      }
+
     return (
-        <div>
-           <form onSubmit={ onSubmit }>
-                <input type="text" name="username" onChange={onChange} />
-                <input type="password" name="password" onChange={onChange}/>
-                <input type="submit" value="회원가입" />
-           </form>
-        </div>
+        <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your username!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+  
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+  
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     )
 }
